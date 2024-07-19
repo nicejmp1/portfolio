2c1091f8acce5598bc839e2aaa7ad4e734951f32
박정민 <163364733+nicejmp1@users.noreply.github.com>
2024-07-19 11:46:55 +0900

    2024

--- js/gsap.js
+++ js/gsap.js
@@ -50,22 +50,73 @@ gsap.utils.toArray(".intro_number").forEach((panel, i) => {
     });
 });
 
-gsap.utils.toArray(".skill__title").forEach((panel, i) => {
-    ScrollTrigger.create({
-        trigger: panel,
-        start: "-=40",
-        endTrigger: "#section4",
-        end: "bottom",
-        pin: true,
-        scrub: 0.5,
-        pinSpacing: false,
-        markers: true
-    });
+
+// 이미지 배열을 초기화합니다.
+const $avatarImgSqc = [];
+
+for (let i = 1; i <= 2; i++) {
+    $avatarImgSqc.push(`/assets/img/face-${i}.jpg`);
+}
+
+// 이미지를 미리 로딩하는 함수입니다.
+function preloading(preImgs) {
+    let imgTotal = preImgs.length;
+    for (let i = 0; i < imgTotal; i++) {
+        let img = new Image();
+        img.src = preImgs[i];
+    }
+}
+
+preloading($avatarImgSqc);
+
+// 이미지 객체와 이미지 태그를 정의합니다.
+const $img = { crntImg: 0 },
+    $imgTag = document.querySelector('.avata');
+
+// TweenMax 애니메이션을 설정합니다.
+let $tween_avata = TweenMax.to($img, 1, {
+    crntImg: $avatarImgSqc.length - 1,
+    roundProps: 'crntImg',
+    immediateRender: true,
+    onUpdate: function () {
+        $imgTag.setAttribute('src', $avatarImgSqc[$img.crntImg]);
+    }
+});
+
+// ScrollMagic 컨트롤러를 정의합니다.
+const $controller = new ScrollMagic.Controller();
+
+// ScrollMagic Scene을 설정합니다.
+new ScrollMagic.Scene({
+    duration: 5000,
+    triggerElement: '#trigger',
+    triggerHook: 0
+})
+    .setTween($tween_avata)
+    .addTo($controller);
+
+gsap.to("#section4 .two__intro .two__number span", {
+    xPercent: 200,
+    ease: "none",
+    scrollTrigger: {
+        trigger: "#section4",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+    }
 });
 
 
-
-
-
-
+gsap.utils.toArray(".text-top").forEach((panel, i) => {
+    ScrollTrigger.create({
+        trigger: panel,
+        start: "top top",
+        endTrigger: ".text2",
+        end: "top",
+        pin: true,
+        markers: true,
+        scrub: 0.5,
+        pinSpacing: false,
+    })
+})
 
