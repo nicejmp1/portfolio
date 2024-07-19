f99b36b54a1cd34bb0b9ca61e245dec4eb1a8e76
박정민 <163364733+nicejmp1@users.noreply.github.com>
2024-07-19 15:38:53 +0900

    2024

--- js/gsap.js
+++ js/gsap.js
@@ -1,40 +1,37 @@
 const targets = gsap.utils.toArray(".split");
 
 targets.forEach((target) => {
-    let SplitClient = new SplitType(target, {
-        type: "lines, words, chars",
-    });
+    let SplitClient = new SplitType(target, { type: "lines, words, chars" });
     let lines = SplitClient.lines;
     let words = SplitClient.words;
     let chars = SplitClient.chars;
+
+    gsap.set(".text h1 .char", { y: 400, opacity: 0 });
+    gsap.set(".text p .char", { opacity: 0, y: 20 });
+
+    setTimeout(() => {
+        const tl = gsap.timeline();
+        tl.to(".text h1 .char", {
+            y: 0,
+            opacity: 1,
+            stagger: {
+                amount: 4,
+                from: "random",
+            },
+            duration: 1,
+            ease: "bounce.out",
+        });
+        tl.to(".text p .char", {
+            opacity: 1,
+            y: 0,
+            stagger: {
+                amount: 1,
+                from: "random",
+            },
+            duration: 3,
+        });
+    }, 1400);
 });
-
-gsap.set(".text h1 .char", { y: 400, opacity: 0 });
-gsap.set(".text p .char", { opacity: 0, y: 20 });
-
-setTimeout(() => {
-    const tl = gsap.timeline();
-    tl.to(".text h1 .char", {
-        y: 0,
-        opacity: 1,
-        stagger: {
-            amount: 4,
-            from: "random",
-        },
-        duration: 1,
-        ease: "bounce.out",
-    });
-    tl.to(".text p .char", {
-        opacity: 1,
-        y: 0,
-        stagger: {
-            amount: 1,
-            from: "random",
-        },
-        duration: 3,
-    });
-}, 1400);
-
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -120,3 +117,69 @@ gsap.utils.toArray(".text-top").forEach((panel, i) => {
     })
 })
 
+gsap.utils.toArray(".project__title").forEach((panel, i) => {
+    ScrollTrigger.create({
+        trigger: panel,
+        start: "top top",
+        end: "+=1100",
+        pin: true,
+        scrub: 0.5,
+        pinSpacing: false,
+        markers: true
+    });
+});
+
+
+
+// 요소 초기 설정 (보이지 않도록 설정)
+gsap.utils.toArray(".test").forEach((text) => {
+    gsap.set(text.querySelectorAll("p"), { yPercent: 100, autoAlpha: 0 });
+    gsap.set(text.querySelectorAll("h2"), { xPercent: -100, autoAlpha: 0 });
+});
+
+// 스크롤 트리거를 사용하여 요소가 나타나게 설정
+gsap.utils.toArray(".test").forEach((text) => {
+    // p 요소 애니메이션
+    gsap.to(text.querySelectorAll("p"), {
+        yPercent: 0,
+        autoAlpha: 1,
+        duration: 3,
+        ease: "circ.out",
+        stagger: {
+            amount: 1,
+            from: "random"
+        },
+        scrollTrigger: {
+            trigger: text,
+            start: "top center",
+            end: "+=500",
+            markers: true,
+            scrub: 1,
+        }
+    });
+
+    // h2 요소 애니메이션
+    gsap.to(text.querySelectorAll("h2"), {
+        xPercent: 0,
+        autoAlpha: 1,
+        duration: 3,
+        ease: "circ.out",
+        stagger: {
+            amount: 1,
+            from: "random"
+        },
+        scrollTrigger: {
+            trigger: text,
+            start: "top center",
+            end: "+=100",
+            markers: true,
+            scrub: 1
+        }
+    });
+});
+
+gsap.to("progress", {
+    value: 100,
+    ease: "none",
+    scrollTrigger: { scrub: 0.3 }
+})
